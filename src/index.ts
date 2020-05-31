62c1802e57de8a39ca513726aacfb5631bb6708f
Charalampos Fanoulis <charalampos.fanoulis@gmail.com>
2020-05-31 23:21:57 +0300

    feat: I think we have command loading now

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export {run} from '@oclif/command';
+import * as yargs from 'yargs';
+import {scan} from 'fs-nextra';
+import {Command} from './base/Command';
+
+(async () => {
+	const cli = yargs
+		.help()
+		.group('help', 'Special:')
+		.version()
+		.alias('version', 'v')
+		.group('version', 'Special:')
+		.completion('completion')
+		.wrap(yargs.terminalWidth())
+		.recommendCommands();
+
+	const dirs = await scan('./commands', {
+		depthLimit: 2,
+		filter: stat => stat.name.endsWith('.js') && stat.isFile()
+	});
+
+	for (const file of [...dirs.keys()]) {
+		// eslint-disable-next-line no-await-in-loop
+		const cmd = await import(file) as unknown as Command<undefined>;
+		cli.command(cmd.cmdString, cmd.description);
+	}
+})();
